4573e77a758a6403c8b4e2211840ce9beca03c05
RockCrafts <ryantanenholz@icloud.com>
2022-04-22 22:31:48 -0400

    heroku2

--- backend/routes/ping.js
+++ backend/routes/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
-    main: function (app, base) {
-      app.get('/ping', (req, res) => {
-       res.send('PONG')
-  });
-}
-}
+  main: function (app, base) {
+    app.get('/ping', (req, res) => {
+      res.send('PONG');
+    });
+  },
+};
